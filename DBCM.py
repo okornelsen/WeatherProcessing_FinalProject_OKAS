237f89cc1b611dc15205106028be8195fe4e1525
Okornelsen <okornelsen@academic.rrc.ca>
2020-11-10 16:55:13 -0600

    Working parsing script for data

--- DBCM.py
+++ DBCM.py
@@ -1,15 +1,27 @@
+'''
+Owen Kornelsen & Ashton Schoffner
+November 09, 2020
+ADEV-3005 Programming in Python
+
+This module uses is a context manager for opening the database
+'''
+
+import sqlite3
+
 class DBCM:
   """
-  This class is a context manager for opening a file
+  This class maanges all components to using a database cursor.
   """
 
-def __init__(self, _filename, _mode):
-  pass
+  def __init__(self, _filename, _mode):
+    self.db = "wpg_weather.sqlite"
 
-def __enter__(self):
-  pass
+  def __enter__(self):
+    self.conn = sqlite3.connect(self.db)
+    self.cur = self.conn.cursor()
 
-def __exit__(self, exc_type, exc_value, exc_trace):
-  pass
+  def __exit__(self, exc_type, exc_value, exc_trace):
+    self.conn.commit()
+    self.conn.close()
 
 
--- scrape_weather.py
+++ scrape_weather.py
@@ -17,25 +17,63 @@ class WeatherScraper(HTMLParser):
 
   def __init__(self):
     HTMLParser.__init__(self)
-    self.weather_data = dict()
+    self.stack = list()
+    self.daily_temps = dict()
+    self.weather = dict()
+    self.count_item = 0
+    self.count_col = 0
+    self.date = ""
 
   def handle_starttag(self, tag, attrs):
-    pass
+    self.stack.append(tag)
+    if tag == "td":
+      self.count_col += 1
 
   def handle_endtag(self, tag):
-    pass
+    if tag == "tr":
+      self.count_col = 0
+      self.count_item = 0
+    while self.stack:
+      item = self.stack.pop()
+      if item == tag:
+        break
 
   def handle_data(self, data):
-    pass
+    if self.stack[-5:] == ["tbody", "tr", "th", "abbr", "a"] or self.stack[-4:] == ["tbody", "tr", "th", "abbr"]:
+      if data.isdigit():
+        self.date = data
+
+    if self.stack[-3:] == ["tbody", "tr", "td"] or self.stack[-2:] == ["td", "a"] and self.count_col <= 3:
+
+      if data == "M" or data == "\xa0":
+        data = "N/A"
+      elif data == "E":
+        return
+
+      if self.count_item == 0:
+        self.daily_temps["Max"] = data
+        self.count_item += 1
+
+      elif self.count_item == 1:
+        self.daily_temps["Min"] = data
+        self.count_item += 1
+
+      elif self.count_item == 2:
+        self.daily_temps["Mean"] = data
+        self.count_item += 1
+
+        if not self.date in self.weather.keys():
+          self.weather[self.date] = self.daily_temps
+          self.daily_temps = dict()
 
   def return_dict_as_formatted_list(self):
-    for keys,values in self.weather_data.items():
+    for keys,values in self.weather.items():
       print(keys,values)
 
 
 myparser = WeatherScraper()
 
-with urllib.request.urlopen('https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year=1996&Month=5') as response:
+with urllib.request.urlopen('https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year=2004&Month=10') as response:
   html = str(response.read())
 
 myparser.feed(html)
